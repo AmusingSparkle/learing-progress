d92b4df73b4fe60057ec381be23baa1ebb333ecd
AmusingSparkle <1193806380@qq.com>
2025-10-25 23:14:49 +0800

    Add files via upload

---
+++ 2/comment.java
@@ -0,0 +1,15 @@
+/*注释包含： 单行注释；
+			多行注释；
+			文档注释；
+而这就是一个多行注释
+*/
+public class comment{
+	public static void main(String[] args){
+		System.out.println("this programm is to learn how to write java's comment");//这是一个单行注释
+	}
+}
+/**这是文档注释
+ * @author Sparkle
+ * @version 1.0
+ * 使用方法为命令框:javadoc -d(这个d一定要写，后面有空格) 文件夹名字 -@... -@... 文件名.java
+ */
