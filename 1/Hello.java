2c0f39dc313af7dd13a08ac0fcaeedba1780ac9e
AmusingSparkle <1193806380@qq.com>
2025-10-25 16:07:49 +0800

    Add files via upload

---
+++ 1/Hello.java
@@ -0,0 +1,22 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}//中文
+class tiger
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello 小老虎~");
+    }
+}
+class cat
+
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello 小猫猫~");
+    }
+}
