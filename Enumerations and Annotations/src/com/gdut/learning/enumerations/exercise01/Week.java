f5e13b51da1bde22ff1b6876d1964ae3696e5ede
AmusingSparkle <1193806380@qq.com>
2025-11-28 23:48:35 +0800

    Add files via upload

---
+++ Enumerations and Annotations/src/com/gdut/learning/enumerations/exercise01/Week.java
@@ -0,0 +1,11 @@
+package com.gdut.learning.enumerations.exercise01;
+
+/**
+ * @author Sparkle
+ * @version 1.0
+ */
+public enum Week {
+    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY;
+    Week() {
+    }
+}
